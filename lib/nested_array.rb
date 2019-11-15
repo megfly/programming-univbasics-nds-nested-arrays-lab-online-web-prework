d7416a0a0fd4cee92911b11ff786e8f394771d19
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-15 18:36:26 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,8 +31,11 @@ def array_literal_matrix
   ar = [ENNET_HOUSE.sort, ENFIELD_TENNIS_ACADEMY.sort]
 end
 
+
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
+  
+  
 end
 
 def matrix_update(matrix, row, column, new_value)
